503e99a8fc5b7e142b2c05d7fe2de63f7f6a270d
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-01 10:50:16 +0530

    content field added

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,13 @@
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm, TextInput,Textarea
 from blog.models import ArticleModel
 class ArticleForm(ModelForm):
     class Meta:
         model = ArticleModel
-        fields = ["title","author","topic"]
+        fields = ["title","author","topic","content"]
         widgets = {
             "title": TextInput(attrs={"placeholder": "Enter title"}),
             "author": TextInput(attrs={"placeholder": "Enter author"}),
             "topic": TextInput(attrs={"placeholder": "Enter topic"}),
+            "content": Textarea(attrs={"placeholder": "Write your content here..","rows":12,"cols":50})
         }
          
